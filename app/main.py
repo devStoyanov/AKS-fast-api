9ee2a6b54660a89de947c028c9a5b539165b4497
Stanislav Stoyanov <stanislav.stoyanov.dev@gmail.com>
2023-09-13 03:50:19 +0300

    AKS second test

--- app/main.py
+++ app/main.py
@@ -48,3 +48,6 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
+@app.delete("/users/")
+def delete_user():
+    return {"message": "working"}
